8098872e6bcc8158aef13d0c23085a5eb9eb35ec
keshavarzm <keshavarzm2017@gmail.com>
2018-11-23 12:25:44 +0900

    2php files

---
+++ privacy-policy.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+	<head>		
+		<script src="https://code.jquery.com/jquery-1.12.4.min.js" ></script>
+		<script>
+			$(document).ready(function(){
+				window.location.replace("https://roundee.io/privacy-policy");
+			});
+			</script>
+	</head>
+	<body>
+		<header></header>
+		<main></main>
+	</body>
+</html>
